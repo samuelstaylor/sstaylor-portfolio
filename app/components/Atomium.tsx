c0acc42b64626cb54e4f3ee410274223eb2feed4
samuelstaylor03 <samuel.s.taylor@vanderbilt.edu>
2026-01-11 00:11:06 -0600

    debug

--- app/components/Atomium.tsx
+++ app/components/Atomium.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useGLTF } from "@react-three/drei";
+import { Vector3 } from "three";
 
-export function Atomium() {
+interface AtomiumProps {
+  position?: [number, number, number];
+  scale?: number;
+}
+
+export function Atomium({ position = [0, 0, 0], scale = 1 }: AtomiumProps) {
   const { scene } = useGLTF("/models/atomium.glb");
-
-  return <primitive object={scene} position={[0, 0, 0]} scale={1} />;
+  return <primitive object={scene} position={position} scale={scale} />;
 }
 
 useGLTF.preload("/models/atomium.glb");
